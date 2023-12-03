48930ef3f35302fe488d065683387984092e649d
nova <novawirananda97@gmail.com>
2023-12-03 20:59:55 +0800

    feat: menambahkan foto program

--- app/Http/Requests/UpdateFotoProgramRequest.php
+++ app/Http/Requests/UpdateFotoProgramRequest.php
@@ -3,6 +3,8 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Http\Exceptions\HttpResponseException;
 
 class UpdateFotoProgramRequest extends FormRequest
 {
@@ -11,7 +13,7 @@ class UpdateFotoProgramRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +24,23 @@ class UpdateFotoProgramRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'program_panti_id' => 'required|exists:program_pantis,id',
+            'nama_foto' => 'required|image|mimes:jpeg,png,jpg,gif|max:2048',
         ];
+    }
+
+    /**
+     * Handle a failed validation attempt.
+     *
+     * @param  \Illuminate\Contracts\Validation\Validator  $validator
+     * @return void
+     *
+     * @throws \Illuminate\Validation\ValidationException
+     */
+    protected function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(
+            response()->json(["error" => $validator->errors(), 'message' => 'terjadi kesalahan dalam validasi'], 422)
+        );
     }
 }
